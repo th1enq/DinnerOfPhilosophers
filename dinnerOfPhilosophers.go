aaef85e697545d701a65d6ee4947fcce8ff0ac69
th1enq <thienchy3305@gmail.com>
2025-03-14 10:16:40 +0700

    fix bug

--- dinnerOfPhilosophers.go
+++ dinnerOfPhilosophers.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"math/rand"
+	"os"
 	"sync"
 	"time"
+)
+
+const (
+	numberOfPhilosophers = 5
 )
 
 const (
@@ -23,6 +30,7 @@ type Philosopher struct {
 	waitTime  time.Duration
 	eatTime   time.Duration
 	state     string
+	logger    *log.Logger
 }
 
 func (p *Philosopher) dine(wg *sync.WaitGroup) {
@@ -32,6 +40,7 @@ func (p *Philosopher) dine(wg *sync.WaitGroup) {
 		p.eat()
 	}
 	p.state = DONE
+	p.logger.Printf("Philosopher %d: Done eating\n", p.id)
 }
 
 func (p *Philosopher) think() {
@@ -39,20 +48,25 @@ func (p *Philosopher) think() {
 	startThink := time.Now()
 	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 	p.thinkTime += time.Since(startThink)
+	p.logger.Printf("Philosopher %d: Thinking\n", p.id)
 }
 
 func (p *Philosopher) eat() {
-	startWait := time.Now()
-	p.state = WAITING
+	p.logger.Printf("Philosopher %d: Waiting\n", p.id)
+
 	p.left.Lock()
+	p.state = WAITING
+	startWait := time.Now()
 	if !p.right.TryLock() {
 		p.left.Unlock()
 		return
 	}
+	// p.right.Lock()
 	p.waitTime += time.Since(startWait)
 
 	p.state = EATING
 	startEat := time.Now()
+	p.logger.Printf("Philosopher %d: Eating\n", p.id)
 	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 	p.eatTime += time.Since(startEat)
 
@@ -62,46 +76,77 @@ func (p *Philosopher) eat() {
 	p.right.Unlock()
 }
 
-func monitor(philosophers []*Philosopher) {
+func monitor(philosophers []*Philosopher, logger *log.Logger) {
 	for {
-		time.Sleep(200 * time.Millisecond)
-		fmt.Println("------------------------")
+		time.Sleep(100 * time.Millisecond)
+		logger.Println("------------------------")
 		for _, p := range philosophers {
-			fmt.Printf("Philosopher %d: %s\n", p.id, p.state)
+			logger.Printf("Philosopher %d: %s\n", p.id, p.state)
 		}
 	}
 }
 
 func main() {
-	n := 5
-	chopticks := make([]*sync.Mutex, n)
+	logFile, err := os.OpenFile("philosophers.log", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Println("Error opening log file:", err)
+		return
+	}
+	defer logFile.Close()
+
+	// MultiWriter: Log to both stdout and file
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+	logger := log.New(multiWriter, "", log.LstdFlags|log.Lmicroseconds)
+
+	chopticks := make([]*sync.Mutex, numberOfPhilosophers)
 	for i := range chopticks {
 		chopticks[i] = &sync.Mutex{}
 	}
 
-	philosophers := make([]*Philosopher, n)
-	for i := 0; i < n; i++ {
+	philosophers := make([]*Philosopher, numberOfPhilosophers)
+	for i := 0; i < numberOfPhilosophers; i++ {
 		philosophers[i] = &Philosopher{
-			id:    i,
-			left:  chopticks[i],
-			right: chopticks[(i+1)%n],
-			state: THINKING,
+			id:     i,
+			left:   chopticks[i],
+			right:  chopticks[(i+1)%numberOfPhilosophers],
+			state:  THINKING,
+			logger: logger,
 		}
 	}
 
 	var wg sync.WaitGroup
-	wg.Add(n)
+	wg.Add(numberOfPhilosophers)
 
-	go monitor(philosophers)
+	// Start monitoring logs
+	go monitor(philosophers, logger)
 
+	// Start philosophers dining
 	for _, p := range philosophers {
 		go p.dine(&wg)
 	}
 
 	wg.Wait()
 
-	fmt.Println("------------ Dinner Done ------------")
+	logger.Println("------------ Dinner Done ------------")
 	for _, p := range philosophers {
-		fmt.Printf("Philosopher %d: Thinking = %v, Waiting = %v, Eating = %v\n", p.id, p.thinkTime, p.waitTime, p.eatTime)
+		logger.Printf("Philosopher %d: Thinking = %v, Waiting = %v, Eating = %v\n", p.id, p.thinkTime, p.waitTime, p.eatTime)
 	}
+
+	averageThinkTime := time.Duration(0)
+	averageWaitTime := time.Duration(0)
+	averageEatTime := time.Duration(0)
+
+	for _, p := range philosophers {
+		averageThinkTime += p.thinkTime
+		averageWaitTime += p.waitTime
+		averageEatTime += p.eatTime
+	}
+
+	averageThinkTime /= time.Duration(numberOfPhilosophers)
+	averageWaitTime /= time.Duration(numberOfPhilosophers)
+	averageEatTime /= time.Duration(numberOfPhilosophers)
+
+	logger.Printf("Average Think Time = %v, Average Wait Time = %v, Average Eat Time = %v\n", averageThinkTime, averageWaitTime, averageEatTime)
+
+	fmt.Println("Logs saved to philosophers.log")
 }
